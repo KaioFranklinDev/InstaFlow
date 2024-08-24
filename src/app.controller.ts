7b264b4980f9ea392fdaaef505a9163ac4db2930
KaioFranklinDev <kaio.franklin.dev@gmail.com>
2024-08-24 12:19:21 -0300

    rota pra recimento de imagens

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors, UploadedFile, Req } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
+  @Get('abacatinho')
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @Post('abacatinho')
+  @UseInterceptors(FileInterceptor('file'))
+  recbePost(@UploadedFile() file: Express.Multer.File, @Req() request: Request) {
+    return this.appService.recbePost({ request, file });
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      mensagem:"hallow"
+    };
+  }
+  recbePost({request , file}) {
+    return {
+      message: 'Recebemos a imagem',
+      filename: file.originalname,
+      mimetype: file.mimetype,
+      size: file.size,
+      body: request.body,
+    };
   }
 }
+
